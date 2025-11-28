a94659e598ee3bf6fe66897258aad4a1095a98b3
Mingzhe <dumingzhex@gmail.com>
2025-11-28 22:32:01 +0800

    fix: update training parameters in grpo_math_8B_test.yaml to improve performance and resource efficiency

---
+++ scripts/train_grpo_single_node.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Exit the script if any command fails
+set -eoux pipefail
+
+# === 1. Slurm Parameters ===
+SLURM_ACCOUNT="root"
+SLURM_PARTITION="main"
+JOB_NAME="grpo-debug"
+CONTAINER_IMAGE="docker://ghcr.io/elfsong/nemo-rl:latest"
+
+# === 2. Environment Variables ===
+export HF_TOKEN=${HF_TOKEN}
+export WANDB_API_KEY=${WANDB_API_KEY}
+export NUM_ACTOR_NODES=1
+export TARGET_NODES='worker-3'
+
+# === 3. Mount Disk ===
+HF_CACHE_DIR="$HOME/.cache/huggingface"
+mkdir -p $HF_CACHE_DIR
+MOUNTS="$PWD:$PWD,$HF_CACHE_DIR:$HF_CACHE_DIR"
+
+# === 4. Command ===
+COMMAND="export HF_TOKEN=$HF_TOKEN && \
+    export WANDB_API_KEY=$WANDB_API_KEY && \
+	uv run ./examples/run_grpo_math.py \
+    --config examples/configs/grpo_math_8B_test.yaml \
+    cluster.num_nodes=${NUM_ACTOR_NODES} \
+    checkpointing.checkpoint_dir='results/llama8b_${NUM_ACTOR_NODES}nodes' \
+    logger.wandb_enabled=True \
+    logger.wandb.name='${JOB_NAME}'"
+
+# === 5. Parameter Checks ===
+echo "Job Name: ${JOB_NAME}"
+
+# === 5. Submit ===
+sbatch \
+        --nodelist=${TARGET_NODES} \
+        --nodes=${NUM_ACTOR_NODES} \
+        --account=${SLURM_ACCOUNT} \
+        --job-name=${JOB_NAME} \
+        --partition=${SLURM_PARTITION} \
+        --exclusive \
+        --mem=0 \
+        --time=1:0:0 \
+        --gres=gpu:8 \
+        --export=ALL,COMMAND="$COMMAND",CONTAINER="$CONTAINER_IMAGE",MOUNTS="$MOUNTS" \
+        ray.sub
